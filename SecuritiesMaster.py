c7325b9c1fb75a1bbbc87005bcc97638510f8cc5
Sharath Chandra Nirmala <sharathnimala@gmail.com>
2023-07-07 00:47:45 +0530

    Update SecuritiesMaster.py

--- SecuritiesMaster.py
+++ SecuritiesMaster.py
@@ -11,13 +11,14 @@ class Master:
     def __init__(self, host : str, port : int, username : str, password : str, progress = False) -> None:
         self.__url = f'postgresql+psycopg2://{username}:{password}@{host}:{port}/securities_master'
         self.__engine = sqlalchemy.create_engine(self.__url, isolation_level='AUTOCOMMIT')
-        #self.__create_base_tables()
+        self.__create_base_tables()
 
     def __create_base_tables(self) -> None:
         for key, command in commands.items():
             with self.__engine.connect() as conn:
                 conn.execute(sql.text(command))
 
+    #NOTE:to be worked on
     def get_data(
         self,
         ticker : str,
